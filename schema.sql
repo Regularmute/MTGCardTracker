dc9e52d842b03b30a94982c8f9d4ac3d1960f8c2
regularmute <93884347+Regularmute@users.noreply.github.com>
2023-03-26 00:16:10 +0200

    style(schema.sql): increase tab size to fit style guidelines

--- schema.sql
+++ schema.sql
@@ -1,28 +1,28 @@
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  name TEXT,
-  password TEXT
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    password TEXT
 );
 
 CREATE TABLE collections (
-  id SERIAL PRIMARY KEY,
-  owner_id INTEGER REFERENCES users,
-  guest_id INTEGER REFERENCES users,
-  name TEXT
+    id SERIAL PRIMARY KEY,
+    owner_id INTEGER REFERENCES users,
+    guest_id INTEGER REFERENCES users,
+    name TEXT
 );
 
 CREATE TABLE decks (
-  id SERIAL PRIMARY KEY,
-  collection_id INTEGER REFERENCES collections,
-  creator_id INTEGER REFERENCES users,
-  name TEXT
+    id SERIAL PRIMARY KEY,
+    collection_id INTEGER REFERENCES collections,
+    creator_id INTEGER REFERENCES users,
+    name TEXT
 );
 
 CREATE TABLE cards (
-  id SERIAL PRIMARY KEY,
-  collection_id INTEGER REFERENCES collections,
-  name TEXT,
-  wins INTEGER,
-  losses INTEGER,
-  colors TEXT
+    id SERIAL PRIMARY KEY,
+    collection_id INTEGER REFERENCES collections,
+    name TEXT,
+    wins INTEGER,
+    losses INTEGER,
+    colors TEXT
 );
