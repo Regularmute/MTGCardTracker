c73f3204e09b4dc005f708e182e0237ea03e8af0
regularmute <93884347+Regularmute@users.noreply.github.com>
2023-03-25 12:19:13 +0200

    test: check that SELECT query works and its result is displayed

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+from os import getenv
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+db = SQLAlchemy(app)
 
 @app.route("/")
 
 def index():
-  return render_template("index.html")
+    result = db.session.execute(text("SELECT name FROM testers"))
+    testers = result.fetchall()
+    return render_template("index.html", count=len(testers), testers=testers)
