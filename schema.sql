79235a3be9a9f0a43b7194aad456c2787c6ceddf
regularmute <93884347+Regularmute@users.noreply.github.com>
2023-03-25 12:39:21 +0200

    fix(schema.sql): syntax error from incorrect commas

--- schema.sql
+++ schema.sql
@@ -1,21 +1,21 @@
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   name TEXT,
-  password TEXT,
+  password TEXT
 );
 
 CREATE TABLE collections (
   id SERIAL PRIMARY KEY,
   owner_id INTEGER REFERENCES users,
   guest_id INTEGER REFERENCES users,
-  name TEXT,
+  name TEXT
 );
 
 CREATE TABLE decks (
   id SERIAL PRIMARY KEY,
   collection_id INTEGER REFERENCES collections,
   creator_id INTEGER REFERENCES users,
-  name TEXT,
+  name TEXT
 );
 
 CREATE TABLE cards (
@@ -24,5 +24,5 @@ CREATE TABLE cards (
   name TEXT,
   wins INTEGER,
   losses INTEGER,
-  colors TEXT,
+  colors TEXT
 );
