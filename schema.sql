ed8a5c856c6ee88d0d74766db6b7511725d62154
regularmute <93884347+Regularmute@users.noreply.github.com>
2023-03-26 11:42:30 +0300

    refactor(schema.sql): collection invites for many-to-many relation

--- schema.sql
+++ schema.sql
@@ -7,8 +7,13 @@ CREATE TABLE users (
 CREATE TABLE collections (
     id SERIAL PRIMARY KEY,
     owner_id INTEGER REFERENCES users,
-    guest_id INTEGER REFERENCES users,
     name TEXT
+);
+
+CREATE TABLE collection_invitations (
+    id SERIAL PRIMARY KEY,
+    collection_id INTEGER REFERENCES collections,
+    guest_id INTEGER REFERENCES users
 );
 
 CREATE TABLE decks (
