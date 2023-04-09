5bfdcfb7298fc4948206e69a3812125948b4c09a
regularmute <93884347+Regularmute@users.noreply.github.com>
2023-04-10 00:27:25 +0300

    feat(invites): add get queries by guest

--- collectioninvites.py
+++ collectioninvites.py
@@ -8,3 +8,14 @@ def invite_user_to_collection(collection_id, guest_id):
     db.session.execute(sql, {"collection_id": collection_id, "guest_id": guest_id})
     db.session.commit()
     return True
+
+def find_guest_in_collection_id(guest_id, collection_id):
+    sql = text("""SELECT guest_id, collection_id FROM collection_invitations
+                WHERE guest_id=:guest_id AND collection_id=:collection_id""")
+    return db.session.execute(
+        sql, {"guest_id": guest_id, "collection_id": collection_id}).fetchall()
+
+def find_collections_by_guest(guest_id):
+    sql = text("""SELECT collection_id FROM collection_invitations
+                WHERE guest_id=:guest_id""")
+    return db.session.execute(sql, {"guest_id": guest_id})
