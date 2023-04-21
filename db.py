a1879bfa818a88de568b531e2266e940659d6e6d
regularmute <93884347+Regularmute@users.noreply.github.com>
2023-04-21 17:38:24 +0300

    fix(flyserver): turn on pre_ping to solve unexpected server timeout

--- db.py
+++ db.py
@@ -3,4 +3,5 @@ from flask_sqlalchemy import SQLAlchemy
 from app import app
 
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1)
+app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {"pool_pre_ping": True}
 db = SQLAlchemy(app)
